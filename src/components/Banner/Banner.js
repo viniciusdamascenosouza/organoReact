493df1ffafaa332b2766ba100aec59e8bb1fa0ee
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-20 16:54:06 -0300

    limpando arquivos desnecessarios

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,9 @@
 import './Banner.css'
 
-function Banner () {
-    //JSX = é como o REACT lê esse "html" e transforma em elementos no DOM
+const Banner = () => {
     return (
         <header className="banner">
-            <img src="/images/banner.png" alt="Banner principal da Organo"/>
+            <img src="/images/banner.png" alt="O banner principal da Organo"/>
         </header>
     )
 }
---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,9 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return(
+        <h1>Olá, Mundo!</h1>
+    )
+}
+
+export default CampoTexto
