58c604bba62a9d26f72e874cea1c079ffc93bffc
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-23 07:36:07 -0300

    descrção JSX

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,9 @@ import './Banner.css'
 
 const Banner = () => {
     return (
+        //JSX é uma extensão React para a sintaxe da linguagem JavaScript que fornece uma 
+        //maneira de estruturar a renderização de componentes usando uma sintaxe familiar a muitos desenvolvedores.
+        //É semelhante em aparência ao HTML.
         <header className="banner">
             <img src="/images/banner.png" alt="O banner principal da Organo"/>
         </header>
