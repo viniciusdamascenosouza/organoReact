88134bb7447e1af44468095cdf0295c6e51491cf
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-23 18:52:19 -0300

    aula2 /3

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -13,14 +13,19 @@ const Formulario = () => {
         'UX e Design',
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome aqui..." />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
+                <CampoTexto obrigatorio="true" label="Nome" placeholder="Digite seu nome aqui..." />
+                <CampoTexto obrigatorio="true" label="Cargo" placeholder="Digite seu cargo..." />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem..." />
-                <ListaSuspensa label="Times" itens={times}/>
+                <ListaSuspensa obrigatorio="true" label="Times" itens={times}/>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
