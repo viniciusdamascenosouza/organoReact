fa47bc3ee78f98d272069be2e42e052c21305807
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-26 16:50:42 -0300

    cadastrando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner.js';
 import Formulario from './components/Formulario/Formulario.js';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
